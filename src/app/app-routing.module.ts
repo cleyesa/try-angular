4386ac8614ff6bb902da84f80137fd5e00d7f165
cleyesa <61216345+cleyesa@users.noreply.github.com>
2020-04-11 14:45:06 +0800

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PokemonSearchComponent } from './pokemon-search/pokemon-search/pokemon-search.component';
-import { PokemonDetailsComponent } from './pokemon-details/pokemon-details/pokemon-details.component';
 
 
-const routes: Routes = [
-  { path: 'pokemons', component: PokemonSearchComponent},
-  { path: 'pokemon-details', component: PokemonDetailsComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-export const routingComponents = [PokemonSearchComponent, PokemonDetailsComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
-import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonsService } from './pokemons.service';
-// import { PokemonSearchComponent } from './pokemon-search/pokemon-search/pokemon-search.component';
-// import { PokemonDetailsComponent } from './pokemon-details/pokemon-details/pokemon-details.component';
+import { PokemonSearchComponent } from './pokemon-search/pokemon-search/pokemon-search.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details/pokemon-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
-    // PokemonSearchComponent,
-    // PokemonDetailsComponent,
+    PokemonSearchComponent,
+    PokemonDetailsComponent,
   ],
   imports: [
     BrowserModule,
